75ab38ea0ef4f16ef1233b6a756911964ae0096e
wesley louis <louiswesley409@gmail.com>
2025-05-18 18:20:28 -0400

    Rebuild with correct backend API URL

--- haiti-virtual-mailbox-frontend/src/services/api.ts
+++ haiti-virtual-mailbox-frontend/src/services/api.ts
@@ -1,10 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL, //
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
-// ✅ Interceptor to automatically attach Authorization header
 api.interceptors.request.use((config) => {
   const userInfo = localStorage.getItem("haitiUser");
   if (userInfo && config.headers) {
